1d30388ef86c519c0b5f14d805f0645fec22b042
MiguelBrav <miguel_98-@hotmail.com>
2025-01-01 22:36:05 -0600

    add user profile tests

---
+++ ToDoList.API/ToDoList.XUnit/UserProfileTests.cs
@@ -0,0 +1,267 @@
+﻿using MediatR;
+using NSubstitute;
+using ToDoList.API.Commands;
+using ToDoList.DTO.ApiResponse;
+using Xunit;
+
+namespace ToDoList.XUnit;
+
+public class UserProfileTests
+{
+    [Theory]
+    [InlineData("urlImage.com")]
+    public async Task SaveUserProfile_ReturnsStatusCode404_WhenUserDoesNotExists(string urlImage)
+    {
+        // Arrange
+        var mediatorSub = Substitute.For<IMediator>();
+        string userId = Guid.NewGuid().ToString(); // UserId mock
+        UserProfileCommand userProfileCommand = new UserProfileCommand()
+        {
+            UrlImage = urlImage,
+            UserId = userId
+        };
+        var apiResponse = new ApiResponse { StatusCode = 404, ResponseMessage = "The user does not exists." };
+
+        mediatorSub.Send(userProfileCommand, default).Returns(Task.FromResult(apiResponse));
+
+        // Act
+        var response = await mediatorSub.Send(userProfileCommand);
+
+        // Assert
+        Assert.NotNull(response);
+        Assert.NotNull(response.ResponseMessage);
+        Assert.Equal(apiResponse.StatusCode, response.StatusCode);
+        Assert.Equal(apiResponse.ResponseMessage, response.ResponseMessage);
+        Assert.IsType<string>(apiResponse.ResponseMessage);
+        Assert.IsType<ApiResponse>(response);
+    }
+
+ 
+    [Theory]
+    [InlineData("urlImage.com")]
+    public async Task SaveUserProfile_ReturnsStatusCode200_WhenResultsAreOk(string urlImage)
+    {
+        // Arrange
+        var mediatorSub = Substitute.For<IMediator>();
+        string userId = Guid.NewGuid().ToString(); // UserId mock
+        UserProfileCommand userProfileCommand = new UserProfileCommand()
+        {
+            UrlImage = urlImage,
+            UserId = userId
+        };
+
+        var apiResponse = new ApiResponse { StatusCode = 200, ResponseMessage = "The image is uploaded successfully." };
+
+        mediatorSub.Send(userProfileCommand, default).Returns(Task.FromResult(apiResponse));
+
+        // Act
+        var response = await mediatorSub.Send(userProfileCommand);
+
+        // Assert
+        Assert.NotNull(response);
+        Assert.NotNull(response.ResponseMessage);
+        Assert.Equal(apiResponse.StatusCode, response.StatusCode);
+        Assert.Equal(apiResponse.ResponseMessage, response.ResponseMessage);
+        Assert.IsType<string>(apiResponse.ResponseMessage);
+        Assert.IsType<ApiResponse>(response);
+    }
+
+    [Theory]
+    [InlineData("urlImage.com")]
+    public async Task SaveUserProfile_ReturnsStatusCode200_WhenUserhasImage(string urlImage)
+    {
+        // Arrange
+        var mediatorSub = Substitute.For<IMediator>();
+        string userId = Guid.NewGuid().ToString(); // UserId mock
+        UserProfileCommand userProfileCommand = new UserProfileCommand()
+        {
+            UrlImage = urlImage,
+            UserId = userId
+        };
+
+        var apiResponse = new ApiResponse { StatusCode = 200, ResponseMessage = "The image already is uploaded." };
+
+        mediatorSub.Send(userProfileCommand, default).Returns(Task.FromResult(apiResponse));
+
+        // Act
+        var response = await mediatorSub.Send(userProfileCommand);
+
+        // Assert
+        Assert.NotNull(response);
+        Assert.NotNull(response.ResponseMessage);
+        Assert.Equal(apiResponse.StatusCode, response.StatusCode);
+        Assert.Equal(apiResponse.ResponseMessage, response.ResponseMessage);
+        Assert.IsType<string>(apiResponse.ResponseMessage);
+        Assert.IsType<ApiResponse>(response);
+    }
+
+    [Theory]
+    [InlineData("urlImage.com")]
+    public async Task UpdateUserProfile_ReturnsStatusCode404_WhenUserDoesNotExists(string urlImage)
+    {
+        // Arrange
+        var mediatorSub = Substitute.For<IMediator>();
+        string userId = Guid.NewGuid().ToString(); // UserId mock
+        UpdateUserProfileCommand userProfileCommand = new UpdateUserProfileCommand()
+        {
+            UrlImage = urlImage,
+            UserId = userId
+        };
+        var apiResponse = new ApiResponse { StatusCode = 404, ResponseMessage = "The user does not exists." };
+
+        mediatorSub.Send(userProfileCommand, default).Returns(Task.FromResult(apiResponse));
+
+        // Act
+        var response = await mediatorSub.Send(userProfileCommand);
+
+        // Assert
+        Assert.NotNull(response);
+        Assert.NotNull(response.ResponseMessage);
+        Assert.Equal(apiResponse.StatusCode, response.StatusCode);
+        Assert.Equal(apiResponse.ResponseMessage, response.ResponseMessage);
+        Assert.IsType<string>(apiResponse.ResponseMessage);
+        Assert.IsType<ApiResponse>(response);
+    }
+
+
+    [Theory]
+    [InlineData("urlImage.com")]
+    public async Task UpdateUserProfile_ReturnsStatusCode200_WhenResultsAreOk(string urlImage)
+    {
+        // Arrange
+        var mediatorSub = Substitute.For<IMediator>();
+        string userId = Guid.NewGuid().ToString(); // UserId mock
+        UpdateUserProfileCommand userProfileCommand = new UpdateUserProfileCommand()
+        {
+            UrlImage = urlImage,
+            UserId = userId
+        };
+
+        var apiResponse = new ApiResponse { StatusCode = 200, ResponseMessage = "The image is updated successfully." };
+
+        mediatorSub.Send(userProfileCommand, default).Returns(Task.FromResult(apiResponse));
+
+        // Act
+        var response = await mediatorSub.Send(userProfileCommand);
+
+        // Assert
+        Assert.NotNull(response);
+        Assert.NotNull(response.ResponseMessage);
+        Assert.Equal(apiResponse.StatusCode, response.StatusCode);
+        Assert.Equal(apiResponse.ResponseMessage, response.ResponseMessage);
+        Assert.IsType<string>(apiResponse.ResponseMessage);
+        Assert.IsType<ApiResponse>(response);
+    }
+
+    [Theory]
+    [InlineData("urlImage.com")]
+    public async Task UpdateUserProfile_ReturnsStatusCode204_WhenUserhasNotImage(string urlImage)
+    {
+        // Arrange
+        var mediatorSub = Substitute.For<IMediator>();
+        string userId = Guid.NewGuid().ToString(); // UserId mock
+        UserProfileCommand userProfileCommand = new UserProfileCommand()
+        {
+            UrlImage = urlImage,
+            UserId = userId
+        };
+
+        var apiResponse = new ApiResponse { StatusCode = 204, ResponseMessage = "The image does not exists." };
+
+        mediatorSub.Send(userProfileCommand, default).Returns(Task.FromResult(apiResponse));
+
+        // Act
+        var response = await mediatorSub.Send(userProfileCommand);
+
+        // Assert
+        Assert.NotNull(response);
+        Assert.NotNull(response.ResponseMessage);
+        Assert.Equal(apiResponse.StatusCode, response.StatusCode);
+        Assert.Equal(apiResponse.ResponseMessage, response.ResponseMessage);
+        Assert.IsType<string>(apiResponse.ResponseMessage);
+        Assert.IsType<ApiResponse>(response);
+    }
+
+    [Fact]
+    public async Task DeleteUserProfile_ReturnsStatusCode404_WhenUserDoesNotExists()
+    {
+        // Arrange
+        var mediatorSub = Substitute.For<IMediator>();
+        string userId = Guid.NewGuid().ToString(); // UserId mock
+        DeleteUserProfileCommand userProfileCommand = new DeleteUserProfileCommand()
+        {
+            UserId = userId
+        };
+        var apiResponse = new ApiResponse { StatusCode = 404, ResponseMessage = "The user does not exists." };
+
+        mediatorSub.Send(userProfileCommand, default).Returns(Task.FromResult(apiResponse));
+
+        // Act
+        var response = await mediatorSub.Send(userProfileCommand);
+
+        // Assert
+        Assert.NotNull(response);
+        Assert.NotNull(response.ResponseMessage);
+        Assert.Equal(apiResponse.StatusCode, response.StatusCode);
+        Assert.Equal(apiResponse.ResponseMessage, response.ResponseMessage);
+        Assert.IsType<string>(apiResponse.ResponseMessage);
+        Assert.IsType<ApiResponse>(response);
+    }
+
+
+    [Fact]
+    public async Task DeleteUserProfile_ReturnsStatusCode200_WhenResultsAreOk()
+    {
+        // Arrange
+        var mediatorSub = Substitute.For<IMediator>();
+        string userId = Guid.NewGuid().ToString(); // UserId mock
+        DeleteUserProfileCommand userProfileCommand = new DeleteUserProfileCommand()
+        {
+             UserId = userId
+        };
+
+        var apiResponse = new ApiResponse { StatusCode = 200, ResponseMessage = "The image is deleted successfully." };
+
+        mediatorSub.Send(userProfileCommand, default).Returns(Task.FromResult(apiResponse));
+
+        // Act
+        var response = await mediatorSub.Send(userProfileCommand);
+
+        // Assert
+        Assert.NotNull(response);
+        Assert.NotNull(response.ResponseMessage);
+        Assert.Equal(apiResponse.StatusCode, response.StatusCode);
+        Assert.Equal(apiResponse.ResponseMessage, response.ResponseMessage);
+        Assert.IsType<string>(apiResponse.ResponseMessage);
+        Assert.IsType<ApiResponse>(response);
+    }
+
+    [Fact]
+    public async Task DeleteUserProfile_ReturnsStatusCode204_WhenUserhasNotImage()
+    {
+        // Arrange
+        var mediatorSub = Substitute.For<IMediator>();
+        string userId = Guid.NewGuid().ToString(); // UserId mock
+        DeleteUserProfileCommand userProfileCommand = new DeleteUserProfileCommand()
+        {
+            UserId = userId
+        };
+
+        var apiResponse = new ApiResponse { StatusCode = 204, ResponseMessage = "The previous user image does not exists." };
+
+        mediatorSub.Send(userProfileCommand, default).Returns(Task.FromResult(apiResponse));
+
+        // Act
+        var response = await mediatorSub.Send(userProfileCommand);
+
+        // Assert
+        Assert.NotNull(response);
+        Assert.NotNull(response.ResponseMessage);
+        Assert.Equal(apiResponse.StatusCode, response.StatusCode);
+        Assert.Equal(apiResponse.ResponseMessage, response.ResponseMessage);
+        Assert.IsType<string>(apiResponse.ResponseMessage);
+        Assert.IsType<ApiResponse>(response);
+    }
+
+
+}
